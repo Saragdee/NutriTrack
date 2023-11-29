50265976992a3d1ba468c9a5456984efdb6ad17e
Edgaras Mackevičius <edasmackis@gmail.com>
2023-11-29 15:27:36 +0200

    Initial commit

---
+++ src/test/java/com/example/NutriTrack/NutriTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NutriTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutriTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
