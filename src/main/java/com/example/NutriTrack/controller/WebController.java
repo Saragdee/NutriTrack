948b5bdf4f65817bd8a554736da4e3a70edb9036
Tali Dovgaluka <vitasykd@gmail.com>
2023-11-30 14:08:42 +0200

    Tali commit #2

---
+++ src/main/java/com/example/NutriTrack/controller/WebController.java
@@ -0,0 +1,21 @@
+package com.example.NutriTrack.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class WebController {
+    @GetMapping("/")
+    public String showIndexPage() {
+        //model.addAttribute("name", "");
+        return "index"; // Returns the name of the HTML page without extension
+    }
+    @GetMapping("/{pageName}")
+    public String showPage(@PathVariable String pageName, Model model) {
+        // Here, the {pageName} variable will match any path segment and be used as the page name
+        model.addAttribute("pageName", pageName);
+        return "template"; // Return the name of the Thymeleaf template
+    }
+}
