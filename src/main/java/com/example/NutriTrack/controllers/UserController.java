38347c95682131126b8a47177bae72f4203702bb
Tali Dovgaluka <vitasykd@gmail.com>
2023-12-05 10:53:34 +0200

    Tali commit test

--- src/main/java/com/example/NutriTrack/controllers/UserController.java
+++ src/main/java/com/example/NutriTrack/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.example.NutriTrack.controllers;
 
+import ch.qos.logback.core.model.Model;
 import com.example.NutriTrack.entities.User;
 import com.example.NutriTrack.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,9 +15,16 @@ public class UserController {
     private UserRepository userRepository;
 
     @GetMapping("/register")
-    public ModelAndView showRegistrationForm(){
+    public ModelAndView showRegistrationForm(Model mopdel){
         ModelAndView mav = new ModelAndView("register");
         mav.addObject("user", new User());
+
+        String[] allergies = {"Peanuts", "Shellfish", "Milk", "Eggs", "Soy", "Wheat", "Fish", "Tree nuts", "Gluten", "Sesame seeds", "Mustard", "Corn", "Lupin", "Celery"};
+        String[] supplements = {"Vitamin D", "Vitamin C", "Calcium", "Iron", "Omega-3 Fatty Acids", "Probiotics", "Multivitamins", "Magnesium", "Zinc", "Biotin", "Melatonin", "Glucosamine"};
+
+        mav.addObject("allergyList", allergies);
+        mav.addObject("supplementList", supplements);
+
         return mav;
     }
 
