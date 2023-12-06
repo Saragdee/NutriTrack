e6ef01358fdd800233f3719bac24e5aff4294ed9
Anastasija Ancigina <andreydostoyevskij@gmail.com>
2023-12-07 01:33:41 +0200

    Food and location API

--- src/main/resources/static/js/registr.js
+++ src/main/resources/static/js/registr.js
@@ -1,16 +1,8 @@
 
-// Replace this with your actual api/database data --------->>>> anastasia to do
 
 $(document).ready(function() {
-    var cities = ['New York', 'Los Angeles', 'London', 'Paris', 'Tokyo']; // Replace with your city data
     var allergies = ['Peanuts', 'Gluten', 'Lactose', 'Pollen']; // Replace with your allergy data
     var supplements = ['Vitamin C', 'Fish Oil', 'Probiotics', 'Calcium']; // Replace with your supplement data
-
-    $('#cityInput').typeahead({
-        source: cities,
-        minLength: 1,
-        items: 'all' // Show all items in the dropdown
-    });
 
     $('#allergiesInput').typeahead({
         source: allergies,
@@ -24,6 +16,7 @@ $(document).ready(function() {
         items: 'all'
     });
 });
+
 (function () {
 'use strict'
 const forms = document.querySelectorAll('.requires-validation')
@@ -39,3 +32,98 @@ Array.from(forms)
     }, false)
   })
 })()
+$(document).ready(function() {
+            const apiKey = 'ed72f3bc92e90d79ab43d3fba25fbe2a'; // Replace with your actual API key
+
+            // Function to fetch city suggestions based on user input
+            function fetchCitySuggestions(query) {
+                const suggestionUrl = `https://api.openweathermap.org/data/2.5/find?q=${query}&type=like&sort=population&cnt=10&appid=ed72f3bc92e90d79ab43d3fba25fbe2a`;
+
+                $.ajax({
+                    url: suggestionUrl,
+                    method: 'GET',
+                    success: function (data) {
+                        displayCitySuggestions(data.list);
+                    },
+                    error: function (error) {
+                        console.error('Error fetching city suggestions:', error);
+                    }
+                });
+            }
+
+            // Function to display city suggestions in the dropdown
+            function displayCitySuggestions(suggestions) {
+                const dropdown = $('#citySuggestions');
+                dropdown.empty();
+
+                suggestions.forEach(city => {
+                    const suggestionItem = `<a class="dropdown-item" href="#" data-id="${city.id}">${city.name}, ${city.sys.country}</a>`;
+                    dropdown.append(suggestionItem);
+                });
+
+                // Show the dropdown
+                dropdown.show();
+            }
+
+            // Handle city input changes
+            $('#cityInput').on('input', function() {
+                const query = $(this).val();
+
+                if (query.length > 2) {
+                    fetchCitySuggestions(query);
+                } else {
+                    $('#citySuggestions').hide();
+                }
+            });
+
+            // Handle city suggestion click
+            $('#citySuggestions').on('click', '.dropdown-item', function() {
+                const cityId = $(this).data('id');
+                const cityName = $(this).text();
+
+                // Set the selected city in the input field
+                $('#cityInput').val(cityName);
+
+                // Hide the suggestions dropdown
+                $('#citySuggestions').hide();
+            });
+
+            // Handle get weather button click
+            $('#getWeatherBtn').on('click', function() {
+                const city = $('#cityInput').val();
+                fetchWeather(city);
+            });
+
+            // Function to fetch weather data for a specific city
+            function fetchWeather(city) {
+                const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ed72f3bc92e90d79ab43d3fba25fbe2a`;
+
+                $.ajax({
+                    url: weatherUrl,
+                    method: 'GET',
+                    success: function (data) {
+                        displayWeather(data);
+                    },
+                    error: function (error) {
+                        console.error('Error fetching weather data:', error);
+                        alert('Error fetching weather data. Please try again.');
+                    }
+                });
+            }
+
+            // Function to display weather results on the page
+            function displayWeather(data) {
+                const weatherResults = $('#weatherResults');
+                weatherResults.empty();
+
+                const weatherHtml = `
+                    <h3>${data.name}, ${data.sys.country}</h3>
+                    <p>Temperature: ${data.main.temp} &deg;C</p>
+                    <p>Weather: ${data.weather[0].description}</p>
+                    <p>Humidity: ${data.main.humidity}%</p>
+                    <p>Wind Speed: ${data.wind.speed} m/s</p>
+                `;
+
+                weatherResults.html(weatherHtml);
+            }
+        });
